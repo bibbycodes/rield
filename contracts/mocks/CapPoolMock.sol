97c1855cb6ddb066cd97faa39d90049b02a47f81
CombatPug <p.ladar@icloud.com>
2023-01-16 00:12:51 +0100

    fix: CAP pool interaction

--- contracts/mocks/CapPoolMock.sol
+++ contracts/mocks/CapPoolMock.sol
@@ -9,6 +9,7 @@ contract CapPoolMock {
     using SafeERC20 for IERC20;
     using Address for address payable;
     uint256 UNIT = 10 ** 18;
+    uint256 CAP_MULTIPLIER = 10 ** 12;
     uint8 decimals = 6;
 
     constructor(address _token, address _rewards) {
@@ -20,7 +21,7 @@ contract CapPoolMock {
     event Withdraw(address indexed user, uint256 amount);
 
     mapping(address => uint256) public deposits;
-    
+
     function deposit(uint256 amount) external payable {
         if (token == address(0)) {
             amount = msg.value;
@@ -33,9 +34,9 @@ contract CapPoolMock {
     }
 
     function getCurrencyBalance(address account) external view returns (uint256) {
-        return deposits[account];
+        return deposits[account] * CAP_MULTIPLIER;
     }
-    
+
     function withdraw(uint256 currencyAmount) external  payable {
         currencyAmount = currencyAmount * (10**decimals) / UNIT;
         deposits[msg.sender] -= currencyAmount;
