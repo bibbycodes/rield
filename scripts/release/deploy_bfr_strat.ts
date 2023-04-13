d2ac5d95fee4aea5a81caece392a5a3f83150b81
CombatPug <p.ladar@icloud.com>
2023-04-13 00:39:25 -0300

    deployed buffer

--- scripts/release/deploy_bfr_strat.ts
+++ scripts/release/deploy_bfr_strat.ts
@@ -7,36 +7,35 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
 
   const Vault = await ethers.getContractFactory("RldTokenVault");
-  const vault = await Vault.deploy();
+  const vault = await Vault.deploy('RLD_BFR', 'RLD_BFR');
   await vault.deployed();
 
   const bfrRewardRouter = '0xbD5FBB3b2610d34434E316e1BABb9c3751567B67'
   const usdcToken = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'
   const bfrToken = '0x1A5B0aaF478bf1FDA7b934c76E7692D722982a6D'
   const unirouter = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
+  const arbToken = '0x912CE59144191C1204E64559FE8253a0e49E6548'
+  const wethToken = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
 
-  const commonAddresses = {
-    vault: vault.address,
-    unirouter,
-    owner: deployer.address,
-  }
-
-  const Strategy = await ethers.getContractFactory("StrategyBFRUniV3");
+  const Strategy = await ethers.getContractFactory("StrategyBFR");
   const strategy = await Strategy.deploy(
     bfrRewardRouter,
-    [usdcToken, bfrToken],
-    [10000],
-    commonAddresses
+    vault.address,
+    unirouter,
+    bfrToken,
+    arbToken,
+    usdcToken,
+    wethToken
   );
   await strategy.deployed();
 
-  await vault.initialize(strategy.address, "RLD_BFR_BFR", "RLD_BFR_BFR")
+  await vault.initStrategy(strategy.address)
 
   console.log("Vault Deployed to:", vault.address);
   console.log("Strategy Deployed to:", strategy.address);
 
   fs.writeFileSync(
-    "./resources/deploy_gmx-output.json",
+    "./resources/deploy_bfr-output.json",
     JSON.stringify({
       vaultAddress: vault.address,
       gmxTokenAddress: bfrToken,
