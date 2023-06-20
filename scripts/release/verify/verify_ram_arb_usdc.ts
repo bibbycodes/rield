1b95fe415e861c3b069f45193fea50e8f12b7ab9
CombatPug <p.ladar@icloud.com>
2023-06-19 23:09:55 -0300

    new vault deploy for solidly vaults

--- scripts/release/verify/verify_ram_arb_usdc.ts
+++ scripts/release/verify/verify_ram_arb_usdc.ts
@@ -3,20 +3,20 @@ import { BigNumber } from 'ethers';
 
 async function main() {
   const [deployer] = await ethers.getSigners();
-  const vaultAddress = '0xa91A36edE47DCF289de86cB6400B16Cf5F079156'
+  const vaultAddress = '0x862d03C462c67AA0A9349662A89495ACe923B6E8'
 
   await hre.run("verify:verify", {
     address: vaultAddress,
     constructorArguments: ["RLD_RAM_ARB_USDC", "RLD_RAM_ARB_USDC"]
   });
 
-  const gauge = '0x2951ffb9e7d54c9ca547901d482f2896e7d9a0ed'
+  const gauge = '0xc43e8F9AE4c1Ef6b8b63CBFEfE8Fe90d375fe11C'
   const lp0 = '0x912ce59144191c1204e64559fe8253a0e49e6548'
   const lp1 = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'
   const WETH = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
-  const router = '0xaaa87963efeb6f7e0a2711f397663105acb1805e'
-  const want = '0xba9f17ca67d1c8416bb9b132d50232191e27b45e'
-  const rewardToken = '0xAAA6C1E32C55A7Bfa8066A6FAE9b42650F262418'
+  const router = '0xf26515d5482e2c2fd237149bf6a653da4794b3d0'
+  const want = '0x9cB911Cbb270cAE0d132689cE11c2c52aB2DedBC'
+  const rewardToken = '0x463913D3a3D3D291667D53B8325c598Eb88D3B0e'
 
   const TokenRoutes = {
     rewardTokenToFeeTokenRoute: [
@@ -44,7 +44,7 @@ async function main() {
   };
 
   await hre.run("verify:verify", {
-    address: '0x5e81d196f891035Eb0CaBEF546856aDDab1CEF09',
+    address: '0xAc4D1Ed3c2fd0fF92566F026d6b45C0AF8E15bD0',
     constructorArguments: [
       vaultAddress,
       want,
